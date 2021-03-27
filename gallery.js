d89fad7f04c3cef84c87714d7ca8a2dc473cbcf4
Dmitriy Nikolaiev <dimnik.u11@gmail.com>
2021-03-27 22:32:40 +0200

    Изменил прокутку изображений, добавил исправления

--- gallery.js
+++ gallery.js
@@ -6,35 +6,45 @@ const lightboxImageRef = document.querySelector('img.lightbox__image');
 
 // Create gallery
 const htmlGalleryItems = galleryItems.map(
-  ({ preview, original, description: alt }) => {
-    return `<li class="gallery__item">
+  ({ preview, original, description: alt }) =>
+    `<li class="gallery__item">
     <a class="gallery__link" href="${original}">
       <img class="gallery__image" src="${preview}"
       data-source="${original}"
       alt="${alt}"/>
     </a>
-  </li>`;
-  },
+  </li>`,
 );
 galleryListRef.insertAdjacentHTML('afterbegin', htmlGalleryItems.join(''));
 
 const closeModal = () => {
   lightboxRef.classList.remove('is-open');
-  lightboxImageRef.setAttribute('src', '');
-  lightboxImageRef.setAttribute('alt', '');
+  lightboxImageRef.src = '';
+  lightboxImageRef.alt = '';
 };
 
 // Lightbox scroll image
 const scrollImage = direction => {
-  let nextImageIndex =
-    galleryItems.findIndex(
-      element => element.original === lightboxImageRef.src,
-    ) + direction;
-  if (nextImageIndex > galleryItems.length - 1) {
-    nextImageIndex = 0;
-  } else if (nextImageIndex < 0) {
-    nextImageIndex = galleryItems.length - 1;
-  }
+  // let nextImageIndex =
+  //   galleryItems.findIndex(
+  //     element => element.original === lightboxImageRef.src,
+  //   ) + direction;
+  // if (nextImageIndex > galleryItems.length - 1) {
+  //   nextImageIndex = 0;
+  // } else if (nextImageIndex < 0) {
+  //   nextImageIndex = galleryItems.length - 1;
+  // }
+  // Отличное решение с вычеслением следующего индекса с использованием остатка от деления.
+  // текущийИндекс + длинаМассива + направление(+1 или -1) % длинаМассива
+  // 1 + 9 + 1: 11 % 9 = 2  если второй и +,    то 3
+  // 8 + 9 + 1: 18 % 9 = 0  если последний и +, то 0
+  // 0 + 9 - 1: 8 % 9 = 2   если первый и -,    то 8
+  // 8 + 0 - 1: 7 % 9 = 7   если последний и -, то 7
+  let nextImageIndex = galleryItems.findIndex(
+    ({ original }) => original === lightboxImageRef.src,
+  );
+  nextImageIndex =
+    (nextImageIndex + galleryItems.length + direction) % galleryItems.length;
   lightboxImageRef.src = galleryItems[nextImageIndex].original;
   lightboxImageRef.alt = galleryItems[nextImageIndex].description;
 };
@@ -43,8 +53,8 @@ const scrollImage = direction => {
 galleryListRef.addEventListener('click', event => {
   event.preventDefault();
   if (event.target.classList.contains('gallery__image')) {
-    lightboxImageRef.setAttribute('src', event.target.dataset.source);
-    lightboxImageRef.setAttribute('alt', event.target.alt);
+    lightboxImageRef.src = event.target.dataset.source;
+    lightboxImageRef.alt = event.target.alt;
     lightboxRef.classList.add('is-open');
   }
 });
